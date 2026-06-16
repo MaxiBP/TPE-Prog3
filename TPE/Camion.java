6ba67d8ebf592234ce59eedf590929428cd3d023
Maximo Bayones <bayonesmaximo20@gmail.com>
2026-06-15 21:07:03 -0300

    constructor, getters y setters

--- TPE/Camion.java
+++ TPE/Camion.java
@@ -1,6 +1,65 @@
 package TPE;
 
+import java.util.List;
+
 public class Camion {
+    private int id;
+    private String patente;
+    private boolean refrigerado;
+    private int capacidadMaxima;
+
+    // para parte 2
+    private int capacidadDisponible;
+    private List<Paquete> paquetesAsignados;
+    
+    //constructor
+    public Camion(int id, String patente, boolean refrigerado, int capacidadMaxima, int capacidadDisponible,
+            List<Paquete> paquetesAsignados) {
+        this.id = id;
+        this.patente = patente;
+        this.refrigerado = refrigerado;
+        this.capacidadMaxima = capacidadMaxima;
+        this.capacidadDisponible = capacidadDisponible;
+        this.paquetesAsignados = paquetesAsignados;
+    }
+    
+    //getters y setters
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getPatente() {
+        return patente;
+    }
+    public void setPatente(String patente) {
+        this.patente = patente;
+    }
+    public boolean isRefrigerado() {
+        return refrigerado;
+    }
+    public void setRefrigerado(boolean refrigerado) {
+        this.refrigerado = refrigerado;
+    }
+    public int getCapacidadMaxima() {
+        return capacidadMaxima;
+    }
+    public void setCapacidadMaxima(int capacidadMaxima) {
+        this.capacidadMaxima = capacidadMaxima;
+    }
+    public int getCapacidadDisponible() {
+        return capacidadDisponible;
+    }
+    public void setCapacidadDisponible(int capacidadDisponible) {
+        this.capacidadDisponible = capacidadDisponible;
+    }
+    public List<Paquete> getPaquetesAsignados() {
+        return paquetesAsignados;
+    }
+    public void setPaquetesAsignados(List<Paquete> paquetesAsignados) {
+        this.paquetesAsignados = paquetesAsignados;
+    }
 
     
 }
--- TPE/Paquete.java
+++ TPE/Paquete.java
@@ -2,5 +2,60 @@ package TPE;
 
 public class Paquete {
 
+    private int id;
+    private String codigo;
+    private int peso;
+    private boolean contieneAlimentos;
+    private int urgencia;
+    
+    //constructor
+    public Paquete(int id, String codigo, int peso, boolean contieneAlimentos, int urgencia) {
+        this.id = id;
+        this.codigo = codigo;
+        this.peso = peso;
+        this.contieneAlimentos = contieneAlimentos;
+        this.urgencia = urgencia;
+    }
+
+    //getters y setters
+    public int getId() {
+        return id;
+    }
+
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public int getPeso() {
+        return peso;
+    }
+
+    public void setPeso(int peso) {
+        this.peso = peso;
+    }
+    public boolean isContieneAlimentos() {
+        return contieneAlimentos;
+    }
+
+    public void setContieneAlimentos(boolean contieneAlimentos) {
+        this.contieneAlimentos = contieneAlimentos;
+    }
+
+    public int getUrgencia() {
+        return urgencia;
+    }
+
+    public void setUrgencia(int urgencia) {
+        this.urgencia = urgencia;
+    }
     
 }
